1e103424f3e63f75db937119c34c20b61314b592
mrk19960617 Lienhard <mark.lienhard@kellogg.northwestern.edu>
2025-01-28 21:46:29 +0000

    lab 1

--- labs/1-data.rb
+++ labs/1-data.rb
@@ -15,3 +15,19 @@
 # rand(1..6)
 # Log output to the screen with puts:
 # puts "Hello, world"
+
+# Simulate rolling two dice
+die1 = rand(1..6)
+die2 = rand(1..6)
+die3 = rand(1..6)
+die4 = rand(1..6)
+
+# Output the rolls
+puts "Roll 1: #{die1}"
+puts "Roll 2: #{die2}"
+puts "Roll 3: #{die3}"
+puts "Roll 4: #{die4}"
+# Calculate and display the total
+total = die1 + die2 + die3 + die4
+puts "Total: #{total}"
+
